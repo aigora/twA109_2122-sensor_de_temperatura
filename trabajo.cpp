7dca387241ed50d3ccb9c1124ea0880aab06a0d4
inesrico <61200052+inesrico@users.noreply.github.com>
2022-03-11 19:27:20 +0100

    Add files via upload

---
+++ trabajo.cpp
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+int menu_ppal(void)
+{
+	float T; // La T de temperatura viene dada por el sensor
+	int tiempo;
+
+	while (tiempo < 100000)
+	{
+		if (tiempo % 30 == 0)
+		{
+			scanf_s("%f", &T);
+			if (T < 22)
+			{
+				printf("La temperatura al tiempo %d es %.2f", tiempo, T);
+				set("LED_ROJO OFF");
+				set("LED_VERDE ON");
+			}
+			else
+			{
+				printf("ALERTA! Se ha superado la temperatura recomendada");
+				set("LED_VERDE OFF");
+				set("LED_ROJO ON");
+			}
+
+			//ALMACENAMOS LA INFORMACION
+		}
+
+	}
+
+}
